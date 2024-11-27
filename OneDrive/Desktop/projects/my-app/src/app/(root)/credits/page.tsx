f3eabb90dc5c9750d1288781f11622fd692e382f
Pranithaiwiz <146003294+Pranith2305@users.noreply.github.com>
2024-11-27 11:47:30 +0530

    Delete OneDrive/Desktop directory

--- OneDrive/Desktop/projects/my-app/src/app/(root)/credits/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const credits = () => {
-  return (
-    <div>credits</div>
-  )
-}
-
-export default credits
--- OneDrive/Desktop/projects/my-app/src/app/(root)/transformation/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const transformation = () => {
-  return (
-    <div>transformation</div>
-  )
-}
-
-export default transformation
